dbb919b3b0ac488a40de0128a28685df95d8a4a3
Prasham1710 <jainprasham17@gmail.com>
2023-03-21 16:42:30 +0530

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BsCalendarDate} from 'react-icons/bs'
+import {MdDevicesOther} from 'react-icons/md'
+import {SlBookOpen} from 'react-icons/sl'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <div className=' w-full grid grid-cols-3 gap-5' >
+      <div className='ml-12'><BsCalendarDate className='cursor-pointer' size={40} color='orange'/><p className='font-bold text-2xl mt-2'>Daily Workouts 7 Days a Week</p>
+      <p className='mt-2 text-gray-500'>Get personalized workouts on a daily basis.</p>
+       <p className=' text-gray-500'> from professionals.</p></div>
+      <div className=''><MdDevicesOther className='cursor-pointer' size={40} color='red'/><p className='font-bold text-2xl mt-2'>Access From Any Platform</p>
+      <p className='mt-2 text-gray-500'>Use all the benefits of the service anywhere </p>
+       <p className=' text-gray-500'> and on any device.</p></div>
+      <div className=''><SlBookOpen className='cursor-pointer' size={40} color='blue'/><p className='font-bold text-2xl mt-2'>Guides & Community</p>
+      <p className='mt-2 text-gray-500'>A private community where we help</p>
+       <p className=' text-gray-500'>each other.</p></div>
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import {RiMenu2Fill} from 'react-icons/ri'
 const Navbar = () => {
   return (
     <div  className='flex pt-8'>
-        <div className=' pl-5'>
+        <div className=' pl-5 '>
         <RiMenu2Fill classname='' fontSize={30} ></RiMenu2Fill></div>
         <div className='flex md:flex md:flex-grow flex-row-reverse space-x-1 pr-6'>
-        <button className='px-[30px] text-xl text-gray-500'> Community</button>
-        <button className='px-[30px] text-xl text-gray-500'> Nutrition Plans</button>
-        <button className='px-[30px] text-xl text-gray-500' >Workouts&Programs</button>
+        <button className='px-[30px] text-xl text-gray-500 hover:underline'> Community</button>
+        <button className='px-[30px] text-xl text-gray-500 hover:underline'> Nutrition Plans</button>
+        <button className='px-[30px] text-xl text-gray-500 hover:underline' >Workouts&Programs</button>
         </div>
         </div>
   )
